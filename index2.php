8f484ed108ba10eee1dcfe4be47cfb95995fd696
ichigo002 <wiktorbojanowski28@wp.pl>
2022-11-23 12:26:11 +0100

    add basic php structure

---
+++ index2.php
@@ -0,0 +1,9 @@
+<?php
+include "parts/head.php";
+include "parts/menu.php";
+
+echo "<br/>To jest index<br/>";
+
+include "parts/footer.php";
+
+?>
---
+++ parts/head.php
@@ -0,0 +1,7 @@
+
+<head>
+    <title>TESTT</title>
+    <style>
+        <?php echo file_get_contents("main-style.css") ?>
+    </style>
+</head>
