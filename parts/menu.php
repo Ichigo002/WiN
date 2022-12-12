e5160f4f95d7cc2a697eab0ded29e2596631a988
ichigo002 <wiktorbojanowski28@wp.pl>
2022-12-12 11:46:23 +0100

    wirk

--- parts/menu.php
+++ parts/menu.php
@@ -1,25 +1,41 @@
 
 <body>
    <header class="header">
-           <script><?php echo file_get_contents("menu-dropdown-script.js"); ?></script>
+         <script><?php echo file_get_contents("menu-dropdown-script.js"); ?></script>
 
-           <!--MENU POTIONS-->
-           <ul class="menu" id="menu">
-               <!--MENU ^^ PODSTAWY -->
-               <li class="menu-item menu-dropdown">
-                  <span class="menu-link" data-toggle="submenu">Podstawy<i class="bx bx-chevron-down"></i></span>
-                  <ul class="submenu">
-                     <li class="submenu-item"><a href=<?php mk_url("dodawanie"); ?> class="submenu-link">Dodawanie</a></li>
-                     <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
-                  </ul>
-               </li>
-               <!--MENU ^^ ZAAWANSOWANE -->
-               <li class="menu-item menu-dropdown">
-                  <span class="menu-link" data-toggle="submenu">Zaawansowane<i class="bx bx-chevron-down"></i></span>
-                  <ul class="submenu">
-                     <li class="submenu-item"><a href=<?php mk_url(""); ?> class="submenu-link">Link</a></li>
-                  </ul>
-               </li>
+         <!--Artiffical fill of flex-->
+         <div></div>
+
+         <!--MENU-->
+         <nav class="menu-nav">
+            <ul class="menu" id="menu">
+                <!--MENU ^^ PODSTAWY -->
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link" data-toggle="submenu">Podstawy<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">
+                      <li class="submenu-item"><a href=<?php mk_url("dodawanie"); ?> class="submenu-link">Dodawanie</a></li>
+                      <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
+                   </ul>
+                </li>
+                <!--MENU ^^ ZAAWANSOWANE -->
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link" data-toggle="submenu">Zaawansowane<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">
+                      <li class="submenu-item"><a href=<?php mk_url(""); ?> class="submenu-link">Link</a></li>
+                   </ul>
+                </li>
             </ul>
+         </nav>
+
+         <!--SEARCH-->
+         <div class="search-bar">
+            <form method="get">
+               <input type="input" placeholder="Search..."/>
+               <input type="submit"/>
+            </form>
+         </div>
       
    </header>
+   <div style="margin-bottom: 35px;">
+      ff
+   </div>
