5d0bf68a63e456e5e444b2e52acf733fe65f4fca
ichigo002 <wiktorbojanowski28@wp.pl>
2022-12-12 11:54:11 +0100

    d

--- parts/menu.php
+++ parts/menu.php
@@ -17,6 +17,14 @@
                       <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
                    </ul>
                 </li>
+                <!--MENU ^^ PODSTAWY -->
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link" data-toggle="submenu">Podstawy<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">
+                      <li class="submenu-item"><a href=<?php mk_url("dodawanie"); ?> class="submenu-link">Dodawanie</a></li>
+                      <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
+                   </ul>
+                </li>
                 <!--MENU ^^ ZAAWANSOWANE -->
                 <li class="menu-item menu-dropdown">
                    <span class="menu-link" data-toggle="submenu">Zaawansowane<i class="bx bx-chevron-down"></i></span>
@@ -35,5 +43,5 @@
       </div>
    </header>
    <div style="margin-bottom: 35px;">
-      ff
+      .
    </div>
