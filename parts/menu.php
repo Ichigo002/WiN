3dcd88ecec55f45b9812d20bee8d41f1534574fa
Nina007504 <pietruszkanina@gmail.com>
2022-12-12 11:44:34 +0100

    Menu klas

--- parts/menu.php
+++ parts/menu.php
@@ -21,6 +21,33 @@
                   </ul>
                </li>
             </ul>
+            <!--MENU KLAS-->
+            <li><a href="#">Start</a></li>
+    <li><a href="#">Portfolio</a>
+    <ul>
+    <li><a href="#">Strony www</a></li>
+    </ul>
+    </li>
+    <li><a href="#">Artykuły</a></li>
+    <li><a href="#">Informacje</a>
+    <ul>
+    <li><a href="#">O mnie</a></li>
+    <li><a href="#">Aktualności</a></li>
+    </ul>
+    </li>
+    <li><a href="#">Webdesign</a></li>
+    <li><a href="#">Kontakt</a>
+    <ul>
+    <li><a href="#">Formularz kontaktowy</a></li>
+    <li><a href="#">Pomoc</a></li>
+    </ul>
+    </li>
+    <li><a href="#">Współpraca</a>
+    <ul>
+    <li><a href="#">Wymiana linkiem</a></li>
+    </ul>
+    </li>
+</ul>
             
       
    </header>
