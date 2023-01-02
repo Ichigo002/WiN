71efeba6c2cf1c3977703c1fe6e798add52d1fb3
Nina007504 <pietruszkanina@gmail.com>
2023-01-02 12:00:53 +0100

    sdfasdfsa

--- subpages/homepage.php
+++ subpages/homepage.php
@@ -2,13 +2,31 @@
       <img src="matematyka.jpg">
    </div>
    <div id="zdj2">
-    <img src="matematyka2.png"
+    <img src="matematyka2.png">
+   </div>
 
-<h1>
-    HOMEPAGE~!
-</h1>
-
-<ul>
+   <nav class="menu-nav">
+            <ul class="menu" id="menu">
+                </li>
+                <!--MENU ^^ PODSTAWY -->
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link" data-toggle="submenu">Klasa 1-3<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">
+                      <li class="submenu-item"><a href=<?php mk_url("dodawanie"); ?> class="submenu-link">Dodawanie</a></li>
+                      <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
+                   </ul>
+                </li>
+                <!--MENU ^^ ZAAWANSOWANE -->
+                <li class="menu-item menu-dropdown">
+                   <span class="menu-link" data-toggle="submenu">Klasa 4-6<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">
+                      <li class="submenu-item"><a href=<?php mk_url(""); ?> class="submenu-link">Kontakt</a></li>
+                      <li class="submenu-item"><a href=<?php mk_url(""); ?> class="submenu-link">O twórcach</a></li>
+                      <li class="submenu-item"><a href=<?php mk_url(""); ?> class="submenu-link">O stronie</a></li>
+                   </ul>
+                </li>
+            </ul>
+         </nav><ul>
     <li><a href=<?php mk_url("dodawanie"); ?>>Link do Dodawania</a></li>
     <li><a href=<?php mk_url("odejmowanie"); ?>>Link do odejmowania</a></li>
     <li><a href=<?php mk_url("1234"); ?>>1234</a></li>
