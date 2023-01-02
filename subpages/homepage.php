350dc39048fc69cbd255fa3c4ca47f851596d1bf
Nina007504 <pietruszkanina@gmail.com>
2023-01-02 12:04:31 +0100

    ijij

--- subpages/homepage.php
+++ subpages/homepage.php
@@ -27,8 +27,6 @@
                 </li>
             </ul>
          </nav><ul>
-    <li><a href=<?php mk_url("dodawanie"); ?>>Link do Dodawania</a></li>
-    <li><a href=<?php mk_url("odejmowanie"); ?>>Link do odejmowania</a></li>
     <li><a href=<?php mk_url("1234"); ?>>1234</a></li>
     <li><a href=<?php mk_url(""); ?>>nothing</a></li>
     <li><a href=<?php mk_url("hp"); ?>>hp</a></li>
