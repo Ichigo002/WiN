a9d43db0e755dd1dd40d101ac978ada3a0671f0d
Nina007504 <pietruszkanina@gmail.com>
2023-01-02 11:04:41 +0100

    asdasd

--- parts/menu.php
+++ parts/menu.php
@@ -11,10 +11,8 @@
             <ul class="menu" id="menu">
                 <!--MENU ^^ PODSTAWY -->
                 <li class="menu-item menu-dropdown">
-                   <span class="menu-link" data-toggle="submenu">Kalkulator<i class="bx bx-chevron-down"></i></span>
-                   <ul class="submenu">
-                      <li class="submenu-item"><a href=<?php mk_url("dodawanie"); ?> class="submenu-link">Dodawanie</a></li>
-                      <li class="submenu-item"><a href=<?php mk_url("odejmowanie"); ?> class="submenu-link">Odejmowanie</a></li>
+                   <a href=<?php mk_url("kalkulator"); ?>span class="menu-link" data-toggle="submenu">Kalkulator<i class="bx bx-chevron-down"></i></span>
+                   <ul class="submenu">sadasd
                    </ul>
                 </li>
                 <!--MENU ^^ PODSTAWY -->
